bb5a5c35552f69358845b72ce1b8c194401b5a21
Rahul Dey <rahuldey9623@gmail.com>
2026-02-23 07:05:07 +0000

    Added comments into bank account class

--- OopsPrinciples/Encapsulation/BankAccount.cs
+++ OopsPrinciples/Encapsulation/BankAccount.cs
@@ -3,17 +3,27 @@ namespace OopsPrinciples.Encapsulation;
 /// <summary>
 /// In this bank account class it encapsulate the account data (balance) and all the related method (deposit, withdraw).
 /// This prevent the direct access of the data (balance) from outside of the class.
-/// 
 /// </summary>
 public class BankAccount
 {
+    /// <summary>
+    /// Private member Balance
+    /// </summary>
     private decimal Balance;
 
+    /// <summary>
+    /// Initialize a bank account instant
+    /// </summary>
+    /// <param name="amount">The amount user want to deposit in the account</param>
     public BankAccount(decimal amount)
     {
         Deposit(amount);
     }
 
+    /// <summary>
+    /// Deposit an amount from the account
+    /// </summary>
+    /// <param name="amount">The amount value</param>
     public void Deposit(decimal amount)
     {
         ValidateAmount(amount);
@@ -21,6 +31,11 @@ public class BankAccount
         Balance += amount;
     }
 
+    /// <summary>
+    /// Withdraw an amount from the account
+    /// </summary>
+    /// <param name="amount">The amount value</param>
+    /// <exception cref="ArgumentException">Argument exception for insufficient funds</exception>
     public void Withdraw(decimal amount)
     {
         ValidateAmount(amount);
@@ -34,11 +49,21 @@ public class BankAccount
         Balance -= amount;
     }
 
+    /// <summary>
+    /// Get current account balance
+    /// </summary>
+    /// <returns>Returns balance value</returns>
     public decimal GetBalance()
     {
         return Balance;
     }
 
+
+    /// <summary>
+    /// Validate if amount is valid or not
+    /// </summary>
+    /// <param name="amount">The amount value</param>
+    /// <exception cref="ArgumentException">Argument exception for invalid amount</exception>
     private static void ValidateAmount(decimal amount)
     {
         if (amount < 0)
